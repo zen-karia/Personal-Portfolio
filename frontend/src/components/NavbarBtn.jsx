c71eeb103d876f294c5849130fa32bb0da8fa28f
zen-karia <149807516+zen-karia@users.noreply.github.com>
2025-04-26 12:26:16 -0400

    Navbar Complete

--- frontend/src/components/NavbarBtn.jsx
+++ frontend/src/components/NavbarBtn.jsx
@@ -4,7 +4,10 @@ import { LuArrowDownRight } from "react-icons/lu";
 export default function NavbarBtn () {
   return (
     <button className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanshadow">
-        Hire Me <LuArrowDownRight />
+        Hire Me 
+        <div className="sm:hidden md:block">
+            <LuArrowDownRight />
+        </div>
     </button>
   )
 }
--- frontend/src/components/NavbarLogo.jsx
+++ frontend/src/components/NavbarLogo.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function NavbarLogo() {
   return (
     <div>
-        <h1 className="text-white text-2xl">Zenil Karia</h1> 
+        <h1 className="text-white text-2xl sm:hidden md:block">Zenil Karia</h1>
+        <h1 className="text-white font-special font-extrabold text-4xl sm:block md:hidden">ZK</h1>
     </div>
   )
 }
