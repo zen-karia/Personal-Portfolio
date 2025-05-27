c949cbf5f6be45115f1f0c42c1cf67d22b62a5a5
zen-karia <149807516+zen-karia@users.noreply.github.com>
2025-05-27 18:07:30 -0400

    "Updated Single Project Section"

--- frontend/src/components/Projects/SingleProject.jsx
+++ frontend/src/components/Projects/SingleProject.jsx
@@ -6,7 +6,7 @@ export const SingleProject = ({ project }) => {
   return (
     <motion.div variants={fadeIn('up', 0.3)} initial='hidden' whileInView='show' viewport={{ once: true, amount: 0}} className="relative flex flex-col items-center text-center p-4 rounded-lg group">
       {/* Project Image */}
-      <div className="relative md:h-[225px] md:w-[500px] sm:w-[475px] sm:h-[225px]">
+      <div className="relative md:h-[225px] md:w-[500px] sm:w-[450px] sm:h-[200px]">
         <div
           className="h-full w-full bg-cover bg-center rounded-lg shadow-lg"
           style={{ backgroundImage: `url(${project.image})` }}
@@ -14,7 +14,7 @@ export const SingleProject = ({ project }) => {
 
         {/* Hover Overlay */}
         <div className="absolute inset-0 bg-black bg-opacity-90 rounded-lg scale-y-0 group-hover:scale-y-100 transition-transform duration-500 origin-center flex flex-col items-center justify-center">
-          <div className="text-white text-md font-bold px-4 text-center">
+          <div className="text-white text-base md:text-md font-bold px-4 text-center">
             {project.description}
           </div>
           <a href={project.link} target='_blank'>{project.button1}</a>
