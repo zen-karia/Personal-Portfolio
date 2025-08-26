5b67bb01f18c601fb2d5d197dadffb73e703dd3c
zen-karia <149807516+zen-karia@users.noreply.github.com>
2025-08-25 21:22:23 -0400

    Updated About Me

--- frontend/src/components/AboutMe/AboutMeText.jsx
+++ frontend/src/components/AboutMe/AboutMeText.jsx
@@ -8,15 +8,22 @@ export const AboutMeText = () => {
           About Me
         </h2>
         <p className='text-white text-2xl'>
-            A CS student @ UWaterloo who loves clean code, messy bugs, 
-            and the dopamine rush of fixing them at 3 a.m. By day, I’m a Software Developer 
-            Intern at eSentire. By night, I’m either training AI models, dancing, performing 
-            magic tricks, or trying to convince Spotify that my gesture-controlled music 
-            player wasn't a mistake. I've built things that talk back (SkillScan), sort trash 
-            smarter than most humans (RecyClean), and even help the visually impaired navigate 
-            the world (BeMyEyes). I write Python like poetry, debug like Sherlock, and dream of 
-            deploying code that never breaks (one day... maybe). Let's build something awesome 
-            — preferably after coffee!! 😁
+           I am a CS student at the University of Waterloo with a strong foundation 
+           in software development, AI, and machine learning. My experience includes 
+           shipping production-ready pipelines at eSentire, conducting research on AI 
+           models for mental health prediction, and building impactful projects such as 
+           SkillScan (an AI-powered resume analyzer), SpeakEz (a wearable ASL glove for 
+           real-time translation), RecyClean (an AI-based waste classification tool), 
+           and BeMyEyes (a wearable assistant for the visually impaired). I am passionate 
+           about developing scalable, reliable, and human-centered software solutions.
+        </p>
+        <br />
+        <p className='text-white text-2xl'>
+           Skilled in Python, JavaScript, Go, React, Node.js, and cloud technologies 
+           (AWS, GCP), I enjoy solving challenging problems and building applications that 
+           make a meaningful impact. Apart from that, my hobbies include skydiving, dancing, 
+           performing magic tricks, exploring AI innovations, and experimenting with creative 
+           side projects. Let’s build something awesome — preferably after a coffee!! 😁
         </p>
         <div>
             <button className='border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white'><Link to="projects" smooth={true} spy={true} duration={500} offset={-160}>Click here to explore some cool stuff that I've made!!! 🚀</Link></button>
