a0107cce66b0715807d2b99b4326c8b5cded5fa8
zen-karia <149807516+zen-karia@users.noreply.github.com>
2026-04-23 12:28:55 -0400

    refactor: update legacy skill components to support dynamic coloring

--- frontend/src/components/Skills/AllSkills.jsx
+++ frontend/src/components/Skills/AllSkills.jsx
@@ -1,136 +1,61 @@
-import React from 'react'
-import { SiHtml5 } from "react-icons/si";
-import { DiCss3 } from "react-icons/di";
-import { IoLogoJavascript } from "react-icons/io";
-import { SiPython } from "react-icons/si";
-import { FaGolang } from "react-icons/fa6";
-import { SiYaml } from "react-icons/si";
-import { GrMysql } from "react-icons/gr";
-import { FaReact } from "react-icons/fa";
-import { FaNodeJs } from "react-icons/fa6";
-import { SiDjango } from "react-icons/si";
-import { FaBootstrap } from "react-icons/fa6";
-import { SiTailwindcss } from "react-icons/si";
-import { SiOpencv } from "react-icons/si";
-import { SiNumpy } from "react-icons/si";
+import React from 'react';
+import { SiHtml5, SiPython, SiYaml, SiDjango, SiOpencv, SiNumpy, SiGnubash, SiTypescript, SiNextdotjs, SiExpress, SiFlask, SiTensorflow, SiKeras, SiPandas, SiScikitlearn, SiGithubactions, SiSnowflake, SiPostgresql, SiPostman } from "react-icons/si";
 import { DiGoogleCloudPlatform } from "react-icons/di";
-import { GrDocker } from "react-icons/gr";
-import { IoLogoGithub } from "react-icons/io";
-import { DiLinux } from "react-icons/di";
-import { SiGnubash } from "react-icons/si";
-import { FaAws } from "react-icons/fa";
-import { TbBrandCpp } from 'react-icons/tb';
+import { IoLogoJavascript } from "react-icons/io";
+import { FaGolang, FaNodeJs } from "react-icons/fa6";
+import { FaReact, FaAws, FaGitAlt, FaCogs, FaMicrophone } from "react-icons/fa";
+import { TbBrandCpp, TbApi, TbBrandReactNative, TbSql } from "react-icons/tb";
+import { GrMysql, GrDocker } from "react-icons/gr";
+
 import { SingleSkill } from './SingleSkill';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../FramerMotion/variants';
 
-const AllSkillsData = [
-  {
-    skill: 'HTML5',
-    icon: SiHtml5,
-    colors: { border: 'border-orange-500', text: 'text-orange-600' },
-  },
-  {
-    skill: 'CSS3',
-    icon: DiCss3,
-    colors: { border: 'border-blue-500', text: 'text-blue-600' },
-  },
-  {
-    skill: 'JavaScript',
-    icon: IoLogoJavascript,
-    colors: { border: 'border-yellow-300', text: 'text-yellow-500' },
-  },
-  {
-    skill: 'Python',
-    icon: SiPython,
-    colors: { border: 'border-yellow-400', text: 'text-blue-500' },
-  },
-  {
-    skill: 'C++',
-    icon: TbBrandCpp,
-    colors: { border: 'border-yellow-400', text: 'text-blue-500' },
-  },
-  {
-    skill: 'Go',
-    icon: FaGolang,
-    colors: { border: 'border-cyan-400', text: 'text-cyan-500' },
-  },
-  {
-    skill: 'YAML',
-    icon: SiYaml,
-    colors: { border: 'border-gray-400', text: 'text-gray-500' },
-  },
-  {
-    skill: 'MySQL',
-    icon: GrMysql,
-    colors: { border: 'border-blue-400', text: 'text-blue-500' },
-  },
-  {
-    skill: 'ReactJS',
-    icon: FaReact,
-    colors: { border: 'border-cyan-400', text: 'text-cyan-500' },
-  },
-  {
-    skill: 'NodeJS/ExpressJS',
-    icon: FaNodeJs,
-    colors: { border: 'border-green-500', text: 'text-green-600' },
-  },
-  {
-    skill: 'Django',
-    icon: SiDjango,
-    colors: { border: 'border-green-600', text: 'text-green-700' },
-  },
-  {
-    skill: 'Bootstrap',
-    icon: FaBootstrap,
-    colors: { border: 'border-purple-500', text: 'text-purple-600' },
-  },
-  {
-    skill: 'TailwindCSS',
-    icon: SiTailwindcss,
-    colors: { border: 'border-blue-400', text: 'text-blue-500' },
-  },
-  {
-    skill: 'Amazon Web Services',
-    icon: FaAws,
-    colors: { border: 'border-yellow-500', text: 'text-yellow-600' }
-  },
-  {
-    skill: 'OpenCV',
-    icon: SiOpencv,
-    colors: { border: 'border-green-400', text: 'text-green-500' },
-  },
-  {
-    skill: 'Numpy',
-    icon: SiNumpy,
-    colors: { border: 'border-blue-400', text: 'text-blue-500' },
-  },
-  {
-    skill: 'Google Cloud Platform',
-    icon: DiGoogleCloudPlatform,
-    colors: { border: 'border-blue-500', text: 'text-blue-600' },
-  },
-  {
-    skill: 'Docker',
-    icon: GrDocker,
-    colors: { border: 'border-blue-400', text: 'text-blue-500' },
-  },
-  {
-    skill: 'Git/GitHub',
-    icon: IoLogoGithub,
-    colors: { border: 'border-gray-500', text: 'text-gray-600' },
-  },
-  {
-    skill: 'Linux',
-    icon: DiLinux,
-    colors: { border: 'border-black', text: 'text-gray-800' },
-  },
-  {
-    skill: 'Bash',
-    icon: SiGnubash,
-    colors: { border: 'border-green-500', text: 'text-green-600' },
-  },
-]
+export const AllSkillsData = [
+  // Languages
+  { skill: 'C/C++', icon: TbBrandCpp, colors: { border: 'border-blue-500', text: 'text-blue-500' } },
+  { skill: 'Python', icon: SiPython, colors: { border: 'border-yellow-400', text: 'text-blue-500' } },
+  { skill: 'JavaScript', icon: IoLogoJavascript, colors: { border: 'border-yellow-300', text: 'text-yellow-500' } },
+  { skill: 'TypeScript', icon: SiTypescript, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+  { skill: 'Go', icon: FaGolang, colors: { border: 'border-cyan-400', text: 'text-cyan-500' } },
+  { skill: 'SQL', icon: TbSql, colors: { border: 'border-blue-300', text: 'text-blue-400' } },
+  { skill: 'HTML/CSS', icon: SiHtml5, colors: { border: 'border-orange-500', text: 'text-orange-600' } },
+  { skill: 'Bash', icon: SiGnubash, colors: { border: 'border-green-500', text: 'text-green-600' } },
+
+  // Frameworks
+  { skill: 'Next.js', icon: SiNextdotjs, colors: { border: 'border-black', text: 'text-gray-800' } },
+  { skill: 'React', icon: FaReact, colors: { border: 'border-cyan-400', text: 'text-cyan-500' } },
+  { skill: 'React Native', icon: TbBrandReactNative, colors: { border: 'border-cyan-400', text: 'text-cyan-500' } },
+  { skill: 'Node.js', icon: FaNodeJs, colors: { border: 'border-green-500', text: 'text-green-600' } },
+  { skill: 'Express.js', icon: SiExpress, colors: { border: 'border-gray-500', text: 'text-gray-600' } },
+  { skill: 'Django', icon: SiDjango, colors: { border: 'border-green-600', text: 'text-green-700' } },
+  { skill: 'Flask', icon: SiFlask, colors: { border: 'border-gray-600', text: 'text-gray-800' } },
+  { skill: 'RESTful APIs', icon: TbApi, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+
+  // Libraries
+  { skill: 'OpenCV', icon: SiOpencv, colors: { border: 'border-red-500', text: 'text-red-500' } },
+  { skill: 'Mediapipe', icon: FaCogs, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+  { skill: 'TensorFlow', icon: SiTensorflow, colors: { border: 'border-orange-500', text: 'text-orange-600' } },
+  { skill: 'Keras', icon: SiKeras, colors: { border: 'border-red-600', text: 'text-red-600' } },
+  { skill: 'NumPy', icon: SiNumpy, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+  { skill: 'Pandas', icon: SiPandas, colors: { border: 'border-purple-500', text: 'text-purple-600' } },
+  { skill: 'Scikit-Learn', icon: SiScikitlearn, colors: { border: 'border-orange-400', text: 'text-orange-500' } },
+  { skill: 'gTTS', icon: FaMicrophone, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+
+  // Development Tools
+  { skill: 'CI/CD', icon: FaCogs, colors: { border: 'border-gray-500', text: 'text-gray-600' } },
+  { skill: 'Git', icon: FaGitAlt, colors: { border: 'border-orange-500', text: 'text-orange-600' } },
+  { skill: 'GitHub Actions', icon: SiGithubactions, colors: { border: 'border-blue-500', text: 'text-blue-600' } },
+  { skill: 'Snowflake', icon: SiSnowflake, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+  { skill: 'AWS', icon: FaAws, colors: { border: 'border-yellow-500', text: 'text-yellow-600' } },
+  { skill: 'MySQL', icon: GrMysql, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+  { skill: 'PostgreSQL', icon: SiPostgresql, colors: { border: 'border-blue-500', text: 'text-blue-600' } },
+  { skill: 'YAML', icon: SiYaml, colors: { border: 'border-gray-400', text: 'text-gray-500' } },
+  { skill: 'GCP', icon: DiGoogleCloudPlatform, colors: { border: 'border-blue-500', text: 'text-blue-600' } },
+  { skill: 'Docker', icon: GrDocker, colors: { border: 'border-blue-400', text: 'text-blue-500' } },
+  { skill: 'Postman', icon: SiPostman, colors: { border: 'border-orange-500', text: 'text-orange-600' } }
+];
+
 export const AllSkills = () => {
   return (
     <div className="relative overflow-hidden w-full h-[300px]">
@@ -140,7 +65,7 @@ export const AllSkills = () => {
             key={index}
             text={item.skill}
             imgSvg={<item.icon />}
-            colors={item.colors} // Pass the colors property
+            colors={item.colors} 
           />
         ))}
         {/* Duplicate the skills for seamless scrolling */}
@@ -149,7 +74,7 @@ export const AllSkills = () => {
             key={`duplicate-${index}`}
             text={item.skill}
             imgSvg={<item.icon />}
-            colors={item.colors} // Pass the colors property
+            colors={item.colors} 
           />
         ))}
       </div>
--- frontend/src/components/Skills/AllSkillsSM.jsx
+++ frontend/src/components/Skills/AllSkillsSM.jsx
@@ -1,117 +1,14 @@
-import React from 'react'
-import { SiHtml5 } from "react-icons/si";
-import { DiCss3 } from "react-icons/di";
-import { IoLogoJavascript } from "react-icons/io";
-import { SiPython } from "react-icons/si";
-import { FaGolang } from "react-icons/fa6";
-import { SiYaml } from "react-icons/si";
-import { GrMysql } from "react-icons/gr";
-import { FaReact } from "react-icons/fa";
-import { FaNodeJs } from "react-icons/fa6";
-import { SiDjango } from "react-icons/si";
-import { FaBootstrap } from "react-icons/fa6";
-import { SiTailwindcss } from "react-icons/si";
-import { SiOpencv } from "react-icons/si";
-import { SiNumpy } from "react-icons/si";
-import { DiGoogleCloudPlatform } from "react-icons/di";
-import { GrDocker } from "react-icons/gr";
-import { IoLogoGithub } from "react-icons/io";
-import { DiLinux } from "react-icons/di";
-import { SiGnubash } from "react-icons/si";
-import { FaAws } from "react-icons/fa";
+import React from 'react';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../FramerMotion/variants';
-import { SingleSkill } from './SingleSkill';
-
-const AllSkillsData = [
-    {
-        skill: 'HTML5',
-        icon: SiHtml5,
-    },
-    {
-        skill: 'CSS3',
-        icon: DiCss3,
-    },
-    {
-        skill: 'JavaScript',
-        icon: IoLogoJavascript,
-    },
-    {
-        skill: 'Python',
-        icon: SiPython,
-    },
-    {
-        skill: 'Go',
-        icon: FaGolang,
-    },
-    {
-        skill: 'YAML',
-        icon: SiYaml,
-    },
-    {
-        skill: 'MySQL',
-        icon: GrMysql,
-    },
-    {
-        skill: 'ReactJS',
-        icon: FaReact,
-    },
-    {
-        skill: 'NodeJS/ExpressJS',
-        icon: FaNodeJs,
-    },
-    {
-        skill: 'Django',
-        icon: SiDjango,
-    },
-    {
-        skill: 'Bootstrap',
-        icon: FaBootstrap,
-    },
-    {
-        skill: 'TailwindCSS',
-        icon: SiTailwindcss,
-    },
-    {
-        skill: 'Amazon Web Services',
-        icon: FaAws,
-    },
-    {
-        skill: 'OpenCV',
-        icon: SiOpencv,
-    },
-    {
-        skill: 'Numpy',
-        icon: SiNumpy,
-    },
-    {
-        skill: 'Google Cloud Platform',
-        icon: DiGoogleCloudPlatform,
-    },
-    {
-        skill: 'Docker',
-        icon: GrDocker,
-    },
-    {
-        skill: 'Git/GitHub',
-        icon: IoLogoGithub,
-    },
-    {
-        skill: 'Linux',
-        icon: DiLinux,
-    },
-    {
-        skill: 'Bash',
-        icon: SiGnubash,
-    },
-]
+import { AllSkillsData } from './AllSkills';
 
 export const AllSkillsSM = () => {
   return (
     <div className='grid md:grid-cols-4 sm:grid-cols-2 gap-12 my-12'>
         {AllSkillsData.map((item, index) => {
             return <motion.div variants={fadeIn("up", 0.2)} initial="hidden" whileInView="show" viewport={{ once: true, amount: 0.7}} key={index} className='flex flex-col items-center'>
-                <item.icon className='text-7xl text-orange'/>
+                <item.icon className={`text-7xl ${item.colors?.text || 'text-orange'}`}/>
                 <p className='text-center mt-4 text-white text-xl'>{item.skill}</p>
             </motion.div>
         })}
